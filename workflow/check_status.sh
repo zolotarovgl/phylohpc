e7900b8185083c85bc0643dded0c311fe7a656a5
Grygoriy Zolotarov <zolotaryovgl@gmail.com>
2025-10-05 15:04:24 +0200

    improved status check

--- workflow/check_status.sh
+++ workflow/check_status.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+#set -u -x
 set -e
+trap 'echo "Undefined variable at line $LINENO: $BASH_COMMAND"' ERR
 
 check_file() {
     local file="$1"
@@ -38,10 +40,12 @@ STATUS_C=$(check_file "$CLU_FILE")
 STATUS_A=$(check_file "$ALN_FILE")
 STATUS_T=$(check_file "$TREE_FILE")
 STATUS_P=$(check_file "$POSSVM_FILE")
-
 # check if the phylogeny has finished 
-if [[ "$STATUS_T" -eq 1 ]]; then
-    STATUS_T=$(grep -c "Consensus tree written" "${TREE_DIR}/${FILEPREF}.log")
+
+if [[ -f "${TREE_DIR}/${FILEPREF}.log" ]]; then
+    STATUS_T=$(grep -c "Consensus tree written" "${TREE_DIR}/${FILEPREF}.log" || true)
+else
+    STATUS_T=0
 fi
 
 echo -e "#prefix\tcluster\talign\ttree\tpossvm"
