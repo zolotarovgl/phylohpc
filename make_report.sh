210bee83558f06637c7d30f87019da87cc013556
Grygoriy Zolotarov <zolotaryovgl@gmail.com>
2026-04-02 18:50:33 +0200

    added

--- make_report.sh
+++ make_report.sh
@@ -1,2 +1,2 @@
-cat data/gene_families_searchinfo.csv | grep -E 'Fork|T-box|Myosin' > tmp/genefam_test.csv
+cat data/gene_families_searchinfo.csv | grep -w -E 'rbp|neu|chr|tfs' > tmp/genefam_test.csv
 python workflow/report_step2.py --results_dir results/ --species_tree data/species_tree.full.newick --family_info tmp/genefam_test.csv  --output report2.html --species_info data/species_info.tsv  --refnames data/Mmus_gene_names.csv  --refsps Mmus --align_dir results/align/
